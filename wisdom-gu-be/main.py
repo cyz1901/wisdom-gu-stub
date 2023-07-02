cce48de5c6fefcba8ba915b9f3dacc1c7413a086
cyz1901 <1093491992@qq.com>
2023-07-02 10:40:57 +0800

    update path

--- wisdom-gu-be/main.py
+++ wisdom-gu-be/main.py
@@ -5,6 +5,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.on_event("startup")
+async def startup_event():
+    # 在这里运行您的代买代码
+    print("Running setup code...")
+    # 这里可以执行您需要的任何自定义操作
+    
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
