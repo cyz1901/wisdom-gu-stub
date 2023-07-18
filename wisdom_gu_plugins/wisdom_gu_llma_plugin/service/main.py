b9cbf0d3d6f74178a4e18a7c56905856047fe65f
cyz1901 <1093491992@qq.com>
2023-07-18 23:07:29 +0800

    gpt4all demo

--- wisdom_gu_plugins/wisdom_gu_llma_plugin/service/main.py
+++
@@ -1,5 +0,0 @@
-from langflow import load_flow_from_json
-
-flow = load_flow_from_json("wisdom_gu_plugins\wisdom_gu_llma_plugin\service\resources\llmaChat.json")
-# Now you can use it like any chain
-flow("Hey, have you heard of Langflow?")
---
+++ wisdom_gu_servcie/main.py
@@ -0,0 +1,20 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.patch("/chat")
+def chat(message: str):
+    
+    return {"answer": "answer"}
